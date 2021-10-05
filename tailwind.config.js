ef6f99e2ef7570761ef3e51699f32c0654140392
Muhammad Azzuhair <15320135+msazzuhair@users.noreply.github.com>
2021-10-05 13:16:37 +0700

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  mode: 'jit',
+  purge: [
+    './dist/*.html',
+    './src/**/*.{js,vue}',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
